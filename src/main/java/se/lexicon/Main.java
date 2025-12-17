9864854a17c18dd93e37f15fc8bc8a5109810428
Halamohamed <halaalimohammed@gmail.com>
2025-12-17 11:14:55 +0100

    Added Order

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,20 +1,40 @@
 package se.lexicon;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
 
-        Customer anna = new Customer(1,"Anna",  "anna@email.com");
-        Customer hala = new Customer(2,"Hala", "hala@email.com");
-        Product product = new Product(1,"Laptop",10200.5);
+        Customer anna = new Customer(1, "Anna", "anna@email.com");
+        Customer hala = new Customer(2, "Hala", "hala@email.com");
+        Product product = new Product(1, "Laptop", 10200.5);
+        Order order1 = new Order(1, anna);
+        ArrayList<Product> productList = new ArrayList<>();
+        productList.add(new Product(1, "Notebook", 10500.95));
+        productList.add(new Product(2, "pen", 2.95));
+        productList.add(new Product(3, "Backpack", 508.5));
+        productList.add(new Product(4, "Water bottle", 305.25));
+        productList.add(new Product(5, "Calculator", 110.5));
+        productList.add(new Product(6, "Headphone", 380.95));
+        productList.add(new Product(7, "Charger", 530.95));
+        productList.add(new Product(8, "Mouse", 300.5));
+        productList.add(new Product(9, "USB cable", 30.95));
+        productList.add(new Product(10, "Planner", 59.3));
+       /* productList.add(new Product(11,"Notebook",10500.95));
+        productList.add(new Product(12,"Notebook",10500.95));
+        productList.add(new Product(13,"Notebook",10500.95));
+*/
+        order1.setProducts(productList);
 
-        IO.println(anna);
-        IO.println(hala);
-        IO.println(product);
-        hala.setEmail("hala.com");
-        hala.setName("");
 
+        /*IO.println(anna);
         IO.println(hala);
+        IO.println(product);*/
+
+        IO.println(order1);
+
+
     }
 }
---
+++ src/main/java/se/lexicon/Order.java
@@ -0,0 +1,66 @@
+package se.lexicon;
+
+import java.util.ArrayList;
+
+public class Order {
+    private int id;
+    private Customer customer;
+    private double totalPrice;
+    private ArrayList<Product> products;
+
+    public Order(int id, Customer customer) {
+        this.id = id;
+        this.customer = customer;
+        this.products = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+    public double calculateTotal(){
+        double total= 0;
+        for (Product p : products){
+            total += p.getPrice();
+        }
+        totalPrice= total;
+        return totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", totalPrice=" + calculateTotal() +
+               // ", products=" + products +
+                '}';
+    }
+}
