ffb329de9f42c96676a4b7b19a69737584e4925f
Halamohamed <halaalimohammed@gmail.com>
2025-12-17 10:39:31 +0100

    Added Product

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -7,9 +7,11 @@ public class Main {
 
         Customer anna = new Customer(1,"Anna",  "anna@email.com");
         Customer hala = new Customer(2,"Hala", "hala@email.com");
+        Product product = new Product(1,"Laptop",10200.5);
 
         IO.println(anna);
         IO.println(hala);
+        IO.println(product);
         hala.setEmail("hala.com");
         hala.setName("");
 
---
+++ src/main/java/se/lexicon/Product.java
@@ -0,0 +1,49 @@
+package se.lexicon;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.isEmpty()){
+            IO.println("The name cannot be empty!");
+        }
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
